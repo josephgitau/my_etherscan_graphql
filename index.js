cebc5d4bdb21507fc2845f8e5a3d22ca816c908c
Joseph Gitau <gitaujoseph64@gmail.com>
2023-11-23 11:51:59 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const { ApolloServer } = require("apollo-server");
+// Import schema from graphql file 
+const { importSchema } = require("graphql-import");
+// Import custom data source 
+const EtherDataSource = require("./datasource/ethDatasource");
+// Import schema
+const typeDefs = importSchema("./schema.graphql");
+
+require("dotenv").config();
+
+// Resolvers map 
+const resolvers = {
+  Query: {
+    etherBalanceByAddress: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.etherBalanceByAddress(),
+
+    totalSupplyOfEther: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.totalSupplyOfEther(),
+
+    latestEthereumPrice: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getLatestEthereumPrice(),
+
+    blockConfirmationTime: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getBlockConfirmationTime(),
+  },
+};
+
+// Create Apollo Server instance
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    ethDataSource: new EtherDataSource(),
+  }),
+});
+
+server.timeout = 0;
+server.listen("9000").then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+});
